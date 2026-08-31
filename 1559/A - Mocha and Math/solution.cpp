a6833c20b857836e04ec638152b06112e695c7b8
Abhay kumar <ak8578227@gmail.com>
2026-08-31 10:53:46 +0530

    Add Mocha and Math [A] from Codeforces

---
+++ 1559/A - Mocha and Math/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		long long ans=-1;
+		for(int i=0;i<n;i++){
+			long long x;
+			cin>>x;
+			if(ans==-1) ans=x;
+			ans=ans&x;
+			
+		}
+		cout<<ans<<endl;
+	}
+}
