6f1540a5b21c1b59268165dc47791de44f2f6c37
Abhay kumar <ak8578227@gmail.com>
2026-08-31 11:06:07 +0530

    Add White-Black Balanced Subtrees [G] from Codeforces

---
+++ 1676/G - White-Black Balanced Subtrees/solution.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <limits>
+#include <random>
+#include <iomanip>
+#include <numeric>
+#include <cassert>
+#include <bitset>
+#include <tuple>
+#include <deque>
+using namespace std;
+ 
+// Type aliases
+using ll    = long long;
+using pii   = pair<int, int>;
+using pll   = pair<ll, ll>;
+using vi    = vector<int>;
+using vll   = vector<ll>;
+using vpii  = vector<pii>;
+using vpll  = vector<pll>;
+using vvi   = vector<vi>;
+ 
+// Constants
+const int MOD    = 1e9 + 7;
+const int INF    = 1e9;
+const ll  LLINF  = 1e18;
+ 
+ 
+// Macros
+#define all(x)            (x).begin(), (x).end()
+#define len(x)            static_cast<int>((x).size())
+#define F                 first
+#define S                 second
+#define pb                push_back
+#define mpr               make_pair
+ 
+// Loop macros
+#define rep(i, a, b)      for (int i = (a); i <= (b); ++i)
+#define per(i, a, b)      for (int i = (b); i >= (a); --i)
+#define repl(i, a, b)     for (ll i = (a); i <= (b); ++i)
+#define perl(i, a, b)     for (ll i = (b); i >= (a); --i)
+ 
+// Fast Exponentiation
+ll mod_exp(ll base, ll exp, ll mod=MOD) {
+    ll result = 1;
+    base %= mod;
+    while (exp > 0) {
+        if (exp & 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+// GCD and LCM
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ 
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+ 
+// sum of all elements in a vector
+template <typename T>
+T sum(const vector<T>& v) {
+    return accumulate(v.begin(), v.end(), T(0));
+}
+// sum of all elements in a 2d vector
+template <typename T>
+T sum(const vector<vector<T>>& v) {
+    T total = 0;
+    for (const auto& row : v) {
+        total += accumulate(row.begin(), row.end(), T(0));
+    }
+    return total;
+}
+// Print a vector
+template <typename T>
+void print(const vector<T>& v) {
+    for (const auto& elem : v) {
+        cout << elem << " ";
+    }
+    cout << endl;
+}
+ 
+// Print a 2D vector
+template <typename T>
+void print(const vector<vector<T>>& v) {
+    for (const auto& row : v) {
+        print(row);
+    }
+}
+ 
+// Print a pair
+template <typename T1, typename T2>
+void print(const pair<T1, T2>& p) {
+    cout << "(" << p.first << ", " << p.second << ")" << endl;
+}
+ 
+// Fast IO
+#define fast_io           ios::sync_with_stdio(false); cin.tie(nullptr)
+ 
+ll dfs(int node, const vector<vector<int>>& adj, const string& s, ll& ans) {
+    ll total = (s[node - 1] == 'W') ? 1 : -1;
+ 
+    for (auto child : adj[node]) {
+        total += dfs(child, adj, s, ans);
+    }
+ 
+    if (total == 0) ans++;
+    return total;
+}
+ 
+void solve() {
+   int n; cin >> n;
+    vector<vector<int>> adj(n + 1);
+    for (int i = 2; i <= n; ++i) {
+        int p; cin >> p;
+        adj[p].push_back(i);
+    }
+    string s; cin >> s;
+    ll ans = 0;
+    dfs(1, adj, s, ans);
+    cout << ans << "
+";
+}
+ 
+int main() {
+    fast_io;
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
