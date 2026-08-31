ee49d148647a835101e2167b871e87e0b735de8e
Abhay kumar <ak8578227@gmail.com>
2026-08-31 10:50:53 +0530

    Add The Third Side [B] from Codeforces

---
+++ 2074/B - The Third Side/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		priority_queue<int, vector<int>, greater<int>> pq;
+		for(int i=0;i<n;i++){
+			int x;
+			cin>>x;
+			pq.push(x);
+		}
+		while(pq.size()!=1){
+			int a=pq.top();
+			pq.pop();
+			int b=pq.top();
+			pq.pop();
+			pq.push((a+b-1));
+		}
+		cout<<pq.top()<<endl;
+	}
+}
