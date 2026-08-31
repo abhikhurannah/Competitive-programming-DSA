bc5b7829c7c9d6facb4628240272626104dcdd09
Abhay kumar <ak8578227@gmail.com>
2026-08-31 11:45:04 +0530

    Add Silhouette [D] from Codeforces

---
+++ 2254/D - Silhouette/solution.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <limits>
+#include <random>
+#include <iomanip>
+#include <numeric>
+#include <cassert>
+#include <bitset>
+#include <tuple>
+#include <deque>
+using namespace std;
+ 
+// Type aliases
+using ll    = long long;
+using pii   = pair<int, int>;
+using pll   = pair<ll, ll>;
+using vi    = vector<int>;
+using vll   = vector<ll>;
+using vpii  = vector<pii>;
+using vpll  = vector<pll>;
+using vvi   = vector<vi>;
+ 
+// Constants
+const int MOD    = 1e9 + 7;
+const int INF    = 1e9;
+const ll  LLINF  = 1e18;
+ 
+ 
+// Macros
+#define all(x)            (x).begin(), (x).end()
+#define len(x)            static_cast<int>((x).size())
+#define F                 first
+#define S                 second
+#define pb                push_back
+#define mpr               make_pair
+ 
+// Loop macros
+#define rep(i, a, b)      for (int i = (a); i <= (b); ++i)
+#define per(i, a, b)      for (int i = (b); i >= (a); --i)
+#define repl(i, a, b)     for (ll i = (a); i <= (b); ++i)
+#define perl(i, a, b)     for (ll i = (b); i >= (a); --i)
+ 
+// Fast Exponentiation
+ll mod_exp(ll base, ll exp, ll mod=MOD) {
+    ll result = 1;
+    base %= mod;
+    while (exp > 0) {
+        if (exp & 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+// GCD and LCM
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ 
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+ 
+// sum of all elements in a vector
+template <typename T>
+T sum(const vector<T>& v) {
+    return accumulate(v.begin(), v.end(), T(0));
+}
+// sum of all elements in a 2d vector
+template <typename T>
+T sum(const vector<vector<T>>& v) {
+    T total = 0;
+    for (const auto& row : v) {
+        total += accumulate(row.begin(), row.end(), T(0));
+    }
+    return total;
+}
+// Print a vector
+template <typename T>
+void print(const vector<T>& v) {
+    for (const auto& elem : v) {
+        cout << elem << " ";
+    }
+    cout << endl;
+}
+ 
+// Print a 2D vector
+template <typename T>
+void print(const vector<vector<T>>& v) {
+    for (const auto& row : v) {
+        print(row);
+    }
+}
+ 
+// Print a pair
+template <typename T1, typename T2>
+void print(const pair<T1, T2>& p) {
+    cout << "(" << p.first << ", " << p.second << ")" << endl;
+}
+ 
+// Fast IO
+#define fast_io           ios::sync_with_stdio(false); cin.tie(nullptr)
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vll b(n);
+    map<ll, int> cnts;
+ 
+    rep(i, 0, n - 1) {
+        cin >> b[i];
+        cnts[b[i]]++;
+    }
+ 
+    if (cnts.find(0) == cnts.end()) {
+        cout << -1 << endl;
+        return;
+    }
+ 
+    vll vec1, vec2;
+    for (auto &[val, count] : cnts) {
+        vec1.pb(val);
+        vec2.pb(count);
+    }
+ 
+    int k = vec1.size();
+    vll x(k);
+    bool flag = false;
+    repl(i, 0, k - 2) {
+        ll diff = vec1[i + 1] -vec1[i];
+        if (diff <= 0 || diff % vec2[i] != 0) {
+            flag = true;
+            break;
+        }
+        x[i] = diff /vec2[i];
+    }
+ 
+    if (k == 1) x[0] = 1;
+    else x[k - 1] = x[k - 2] + 1;
+    
+    if (flag || x[0] < 1) {
+        cout << -1 << "
+";
+        return;
+    }
+    repl(i, 0, k - 2) {
+        if (x[i] >=x[i + 1]) {
+            cout << -1 << endl;
+            return;
+        }
+    }
+    map<ll, ll> mp;
+    repl(i, 0, k - 1) mp[vec1[i]] = x[i];
+    repl(i, 0, n - 1) cout << mp[b[i]] << " ";
+    cout << endl;
+}
+ 
+int main() {
+    fast_io;
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
