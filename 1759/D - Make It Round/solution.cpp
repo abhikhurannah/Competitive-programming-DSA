d59e01d9b8e04b9cbbfc2d6267a71eeb1a76685e
Abhay kumar <ak8578227@gmail.com>
2026-08-31 11:10:33 +0530

    Add Make It Round [D] from Codeforces

---
+++ 1759/D - Make It Round/solution.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <limits>
+#include <random>
+#include <iomanip>
+#include <numeric>
+#include <cassert>
+#include <bitset>
+#include <tuple>
+#include <deque>
+using namespace std;
+ 
+// Type aliases
+using ll    = long long;
+using pii   = pair<int, int>;
+using pll   = pair<ll, ll>;
+using vi    = vector<int>;
+using vll   = vector<ll>;
+using vpii  = vector<pii>;
+using vpll  = vector<pll>;
+using vvi   = vector<vi>;
+ 
+// Constants
+const int MOD    = 1e9 + 7;
+const int INF    = 1e9;
+const ll  LLINF  = 1e18;
+ 
+ 
+// Macros
+#define all(x)            (x).begin(), (x).end()
+#define len(x)            static_cast<int>((x).size())
+#define F                 first
+#define S                 second
+#define pb                push_back
+#define mpr               make_pair
+ 
+// Loop macros
+#define rep(i, a, b)      for (int i = (a); i <= (b); ++i)
+#define per(i, a, b)      for (int i = (b); i >= (a); --i)
+#define repl(i, a, b)     for (ll i = (a); i <= (b); ++i)
+#define perl(i, a, b)     for (ll i = (b); i >= (a); --i)
+ 
+// Fast Exponentiation
+ll mod_exp(ll base, ll exp, ll mod=MOD) {
+    ll result = 1;
+    base %= mod;
+    while (exp > 0) {
+        if (exp & 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+// GCD and LCM
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ 
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+ 
+// sum of all elements in a vector
+template <typename T>
+T sum(const vector<T>& v) {
+    return accumulate(v.begin(), v.end(), T(0));
+}
+// sum of all elements in a 2d vector
+template <typename T>
+T sum(const vector<vector<T>>& v) {
+    T total = 0;
+    for (const auto& row : v) {
+        total += accumulate(row.begin(), row.end(), T(0));
+    }
+    return total;
+}
+// Print a vector
+template <typename T>
+void print(const vector<T>& v) {
+    for (const auto& elem : v) {
+        cout << elem << " ";
+    }
+    cout << endl;
+}
+ 
+// Print a 2D vector
+template <typename T>
+void print(const vector<vector<T>>& v) {
+    for (const auto& row : v) {
+        print(row);
+    }
+}
+ 
+// Print a pair
+template <typename T1, typename T2>
+void print(const pair<T1, T2>& p) {
+    cout << "(" << p.first << ", " << p.second << ")" << endl;
+}
+ 
+// Fast IO
+#define fast_io           ios::sync_with_stdio(false); cin.tie(nullptr)
+ 
+int cnt_fac(ll x, ll factor) {
+    int count = 0;
+    while (x % factor == 0 && x > 0) {
+        count++;
+        x /= factor;
+    }
+    return count;
+}
+ 
+void solve() {
+    ll n, m;
+    cin >> n >> m;
+    ll ans = n;
+    ll cnt2 = cnt_fac(n, 2);
+    ll cnt5 = cnt_fac(n, 5);
+    ll num = 1;
+    if(cnt2 == cnt5){
+        while(num* 10 <= m){
+            num*=10;
+        }
+    }else if(cnt2 < cnt5){
+        while(cnt2 < cnt5 && num * 2 <= m){
+            num *= 2;
+            cnt2++;
+        }
+        while(num * 10 <= m){
+            num*=10;
+        }
+    } else {
+        while(cnt5 < cnt2 && num * 5 <= m){
+            num *= 5;
+            cnt5++;
+        }
+        while(num * 10 <= m){
+            num*=10;
+        }
+    }
+    num *= (m / num);
+    cout << n * num << '
+';
+}
+ 
+ 
+int main() {
+    fast_io;
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
