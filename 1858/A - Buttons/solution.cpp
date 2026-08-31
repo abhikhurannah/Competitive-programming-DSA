abe5cba8d6e97a0ba9280258e3351ffbad2d4610
Abhay kumar <ak8578227@gmail.com>
2026-08-31 10:48:59 +0530

    Add Buttons [A] from Codeforces

---
+++ 1858/A - Buttons/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	   long long a,b,c;
+	   cin>>a>>b>>c;
+	   if(c%2!=0) a=a+1;
+	   if(a>b) cout<<"First"<<endl;
+	   else cout<<"Second"<<endl;
+	   
+	}
+}
