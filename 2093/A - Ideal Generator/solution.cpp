1bbf98af6f31132f28e15016c5ade425d02350ad
Abhay kumar <ak8578227@gmail.com>
2026-08-31 10:52:06 +0530

    Add Ideal Generator [A] from Codeforces

---
+++ 2093/A - Ideal Generator/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int t;
+    cin >> t;
+    while(t--){
+        int k;
+        cin >> k;
+        
+        if(k % 2 == 1)
+            cout << "YES"<<endl;
+        else
+            cout << "NO"<<endl;;
+    }
+    
+}
